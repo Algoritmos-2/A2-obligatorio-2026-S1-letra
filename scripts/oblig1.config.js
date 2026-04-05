8eae6d86526204fd4a1124bf5a3383600fef2e8a
Joaquin Vigna <joaquinvigna@pm.me>
2026-04-05 19:58:23 -0300

    export pdf template universitario

---
+++ scripts/oblig1.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // Institutional info
+  materia: "Estructuras de Datos y Algoritmos 2",
+  carrera: "Ingeniería en Sistemas",
+  evaluacion: "Obligatorio 1",
+  grupo: "",
+  fecha_entrega: "25/05/2026",
+  fecha_defensa: "25/06/2026",
+  puntaje_maximo: "20",
+  puntaje_minimo: "0",
+
+  // Exercises included in this obligatorio
+  ejercicios: [1, 2, 3, 4, 5],
+
+  // Extra pages to include (in order, after exercises)
+  extraPages: ["formato", "documentacion", "autoria", "defensa"],
+
+  // Output
+  outputFile: "obligatorio1.pdf",
+};
---
+++ scripts/oblig2.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // Institutional info
+  materia: "Estructuras de Datos y Algoritmos 2",
+  carrera: "Ingeniería en Sistemas",
+  evaluacion: "Obligatorio 2",
+  grupo: "",
+  fecha_entrega: "25/06/2026",
+  fecha_defensa: "25/06/2026",
+  puntaje_maximo: "20",
+  puntaje_minimo: "0",
+
+  // Exercises included in this obligatorio
+  ejercicios: [6, 7, 8, 9, 10],
+
+  // Extra pages to include (in order, after exercises)
+  extraPages: ["formato", "documentacion", "autoria", "defensa"],
+
+  // Output
+  outputFile: "obligatorio2.pdf",
+};
